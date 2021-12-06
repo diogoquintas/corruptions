12028d0a83ac031abce2ab3cb43608236d64854d
Diogo Quintas <odiogoribeiroq@gmail.com>
2021-12-06 12:39:05 +0000

    feat: add contract details

--- components/contract-item/ContractItem.js
+++ components/contract-item/ContractItem.js
@@ -1,9 +1,15 @@
-export default function ContractItem({ title, address }) {
+import { useState } from "react";
+
+export default function ContractItem({ title, address, description }) {
+  const [open, setOpen] = useState(false);
+
   return (
     <li key={address}>
       <a target="_blank" href={`https://etherscan.io/address/${address}`}>
         {title}
       </a>
+      <button onClick={() => setOpen(!open)}>{`${open ? "-" : "+"}`}</button>
+      {description && open && <p>{description}</p>}
     </li>
   );
 }
--- components/contract-list/ContractList.js
+++ components/contract-list/ContractList.js
@@ -5,64 +5,77 @@ export default function ContractList() {
     <section>
       <h3>Contracts</h3>
       <section>
-        <h4>dom</h4>
+        <h4>from dom</h4>
         <ul>
           <ContractItem
             title="Corruptions"
             address="0x5BDf397bB2912859Dbd8011F320a222f79A28d2E"
+            description="The main contract. The ERC-721 interface."
           />
           <ContractItem
             title="CorruptionsMetadata"
             address="0x2688212B769B693254D054f7E4b0d86E9462373b"
-          />
-          <ContractItem
-            title="CorruptionsFont"
-            address="0xdf8A48979F33f12952Ab4cF6f960EA4071fc656b"
+            description="The contract that generates the token data + image for corruptions."
           />
           <ContractItem
             title="CorruptionsDataMapper"
             address="0x7A96d95a787524a27a4df36b64a96910a2fDCF5B"
-          />
-          <ContractItem
-            title="CorruptionsTemporalFragmentDeviationWriter"
-            address="0x6a00bC9eD4DcE14ed7bA72D01da6ed4746C5792e"
+            description="Additional information to each corruption, at the moment maps the tokenId with his deviation status."
           />
           <ContractItem
             title="CorruptionsWiltedRoseDeviationWriter"
             address="0x4500f6e46D485B62eD35926052F04C04f627F637"
+            description="The first deviation chapter. The wilted rose, only 64 could deviate."
+          />
+          <ContractItem
+            title="CorruptionsTemporalFragmentDeviationWriter"
+            address="0x6a00bC9eD4DcE14ed7bA72D01da6ed4746C5792e"
+            description="The second deviation chapter. Contains 8 different deviations."
           />
           <ContractItem
             title="CorruptionsDeviationRegistry"
             address="0x40f7ad19c7F37AcD76958d61Cbc783c77411cd9b"
-          />
-          <ContractItem
-            title="CorruptionsPaletteToCharacterHelper"
-            address="0x4680bdeB308ce794Ed1Bb86162f8d81ae8477ba8"
+            description="The names of each deviation, their current contract address and linking between tokenId and deviation."
           />
           <ContractItem
             title="CorruptionsDataChannel"
             address="0x90Ccc00699222A9870DbCFb980512a6eA40edB6F"
+            description="The portal that receives messages from (*) and (s)."
           />
           <ContractItem
             title="CorruptionsStrangeApparatus"
             address="0xe6F3E72d039563BE214B2762Df39049ce20D2426"
+            description="The portal that receives ciphers."
+          />
+          <ContractItem
+            title="CorruptionsPaletteToCharacterHelper"
+            address="0x4680bdeB308ce794Ed1Bb86162f8d81ae8477ba8"
+            description="Render utils."
+          />
+          <ContractItem
+            title="CorruptionsFont"
+            address="0xdf8A48979F33f12952Ab4cF6f960EA4071fc656b"
+            description="Render utils."
           />
         </ul>
       </section>
       <section>
-        <h4>community</h4>
+        <h4>from the community</h4>
         <ul>
           <ContractItem
-            address="0xFEEDa52dc1c570533B68eFC9a6DaA2D212bCC836"
-            title="ReflectionsDataChannel (from the community)"
-          />
-          <ContractItem
             address="0x06EB2100d66558CF8BCc3784420ad549D8D69bf6"
-            title="Reflections (from the community)"
+            title="Reflections"
+            description="NFT to fund the 8 deviations reserved for our multisig wallet."
           />
           <ContractItem
-            address="0x7572f8cC39266AEa2A29cAd3536C8f8904a599f8"
-            title="ReflectionsMetadata (from the community)"
+            address="0x86a103d80798A9202CbA0cEc9F499Cc24eA24FA5"
+            title="ReflectionsMetadata"
+            description="The contract that generates the token data + image for reflections."
+          />
+          <ContractItem
+            address="0xFEEDa52dc1c570533B68eFC9a6DaA2D212bCC836"
+            title="ReflectionsDataChannel"
+            description="The portal created to talk with (*) and (s)."
           />
         </ul>
       </section>
