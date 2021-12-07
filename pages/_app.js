748469f99a5e1f38e924a845d9db56682cf21193
Diogo Quintas <odiogoribeiroq@gmail.com>
2021-12-07 17:27:59 +0000

    fix: fix title

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/dist/client/router";
 import Link from "next/link";
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
@@ -7,21 +8,37 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <div>
+      <Head>
+        <title>Corruption(s*)</title>
+      </Head>
       <nav style={{ display: "flex", gap: "10px" }}>
         <Link href="/">
-          <a style={{ color: pathname === "/" ? "orangered" : "black" }}>
+          <a
+            style={{
+              color: pathname === "/" ? "orangered" : "black",
+              textDecoration: "none",
+            }}
+          >
             home
           </a>
         </Link>
         <Link href="/contracts">
           <a
-            style={{ color: pathname === "/contracts" ? "orangered" : "black" }}
+            style={{
+              color: pathname === "/contracts" ? "orangered" : "black",
+              textDecoration: "none",
+            }}
           >
             contracts
           </a>
         </Link>
         <Link href="/cipher">
-          <a style={{ color: pathname === "/cipher" ? "orangered" : "black" }}>
+          <a
+            style={{
+              color: pathname === "/cipher" ? "orangered" : "black",
+              textDecoration: "none",
+            }}
+          >
             cipher
           </a>
         </Link>
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,9 @@
-import Head from "next/head";
 import corruption from "../corruption";
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Corruption(s*)</title>
-      </Head>
-      <main>
-        <img
-          style={{ marginTop: "10px" }}
-          width="320"
-          src={corruption}
-          alt=""
-        />
-      </main>
-    </div>
+    <main>
+      <img style={{ marginTop: "10px" }} width="320" src={corruption} alt="" />
+    </main>
   );
 }
