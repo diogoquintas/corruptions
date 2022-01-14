508508551179ae496811e6a080800f4e97341a5f
Diogo Quintas <odiogoribeiroq@gmail.com>
2022-01-14 17:52:08 +0000

    fix: fix about

--- components/about/About.js
+++ components/about/About.js
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 import Link from "next/link";
-import wiltedFlower from "../../gallery/wilted-flower";
+import wiltedRose from "../../gallery/wilted-rose";
 
 const Section = styled.section`
   display: flex;
@@ -120,10 +120,10 @@ export default function About() {
           </abbr>
           . Followers of Dom, noticed the contract and rushed to{" "}
           <abbr title="Bring an NFT to life.">mint</abbr> the corruptions. The
-          artworks were cryptic looking and had a weird mechanic called
-          "insight" which would grow in time, decreasing in aceleration if the
-          corruption was transferred ownership. From then on, other contracts
-          were deployed by Dom, one in particular called{" "}
+          artworks were cryptic looking and had a misterious mechanic called
+          "insight" which appeared to be growing overtime with an acceleration
+          that would decrease if the corruption was transfered. From then on,
+          other contracts were deployed by Dom, one in particular called{" "}
           <Link
             href={`https://etherscan.io/address/${process.env.NEXT_PUBLIC_DATA_CHANNEL_ADDRESS}`}
             passHref
@@ -132,10 +132,8 @@ export default function About() {
               CorruptionsDataChannel
             </A>
           </Link>
-          . During this, the creator never explained or promoted the project
-          publicly, so, the role to understand and figure out what each contract
-          did was delegated to the community. A group of like-minded people
-          interested in the project created a{" "}
+          . All of this was happening without any explanation from Dom, only
+          code. The people trying to figure out what was going on created a{" "}
           <Link
             aria-label="Community discord"
             title="Community discord"
@@ -146,11 +144,11 @@ export default function About() {
               discord channel
             </A>
           </Link>{" "}
-          to figure out this project and share ideias.
+          where they could share ideas.
         </dd>
         <dt>Chapter 1</dt>
         <dd>
-          From the contract{" "}
+          One of the deployed contracts, the{" "}
           <Link
             href={`https://etherscan.io/address/${process.env.NEXT_PUBLIC_DATA_CHANNEL_ADDRESS}`}
             passHref
@@ -159,8 +157,8 @@ export default function About() {
               CorruptionsDataChannel
             </A>
           </Link>{" "}
-          appeared cryptic messages and the community eager to understand the
-          meaning of it, created a mirror contract called{" "}
+          started emitting cryptic messages, the community, eager to understand,
+          created a duplicated contract called{" "}
           <Link
             href={`https://etherscan.io/address/${process.env.NEXT_PUBLIC_REFLECTIONS_DATA_CHANNEL_ADDRESS}`}
             passHref
@@ -169,52 +167,49 @@ export default function About() {
               ReflectionsDataChannel
             </A>
           </Link>{" "}
-          to communicate back with this "entity". From those{" "}
+          and started asking questions. From this interaction a narrative was
+          born.{" "}
           <Link href="/data-channel" passHref>
-            <A>messages</A>
-          </Link>{" "}
-          a narrative started to grow.
+            <A>Read the messages</A>
+          </Link>
+          .
         </dd>
         <dt>Deviations, what are those?</dt>
         <dd>
-          During chapter 1, there were 2 oportunities to deviate a corruption,
-          once deviated a corruption can <b>never turn back</b>.
-          <P>
-            <b>The first one</b>, through the contract{" "}
-            <Link
-              href="https://etherscan.io/address/0x4500f6e46D485B62eD35926052F04C04f627F637"
-              passHref
-            >
-              <A target="_blank" rel="noreferrer">
-                CorruptionsWiltedRoseDeviationWriter
-              </A>
-            </Link>{" "}
-            you could transform your corruption into a wilted flower, only 64
-            could deviate.{" "}
-            <ImageWrapper>
-              <Img src={wiltedFlower} alt="" />
-              <span>The wilted flower 0xF30</span>
-            </ImageWrapper>
-          </P>
-          <P>
-            <b>The second one</b>, through the contract{" "}
-            <Link
-              href="https://etherscan.io/address/0x6a00bC9eD4DcE14ed7bA72D01da6ed4746C5792e"
-              passHref
-            >
-              <A target="_blank" rel="noreferrer">
-                CorruptionsTemporalFragmentDeviationWriter
-              </A>
-            </Link>{" "}
-            you could transform your corruption into one of 8 (randomly
-            selected) unique artworks which were later used as pieces of the{" "}
-            <Link href="/timeline" passHref>
-              <A>timeline</A>
-            </Link>
-            .
-          </P>
+          During chapter 1, there were 2 oportunities to deviate a corruption.
+          <br />
+          <b>Once deviated a corruption can never turn back</b>.{" "}
+          <Link
+            href="https://etherscan.io/address/0x4500f6e46D485B62eD35926052F04C04f627F637"
+            passHref
+          >
+            <A target="_blank" rel="noreferrer">
+              The first deviation
+            </A>
+          </Link>{" "}
+          transformed your corruption into a wilted rose. Only 64 could deviate.{" "}
+          <ImageWrapper>
+            <Img src={wiltedRose} alt="" />
+            <span>The wilted rose 0xF30</span>
+          </ImageWrapper>{" "}
+          <Link
+            href="https://etherscan.io/address/0x6a00bC9eD4DcE14ed7bA72D01da6ed4746C5792e"
+            passHref
+          >
+            <A target="_blank" rel="noreferrer">
+              The second one
+            </A>
+          </Link>{" "}
+          transformed your corruption into one of 8 (randomly selected) unique
+          artworks which were later used as pieces of the{" "}
+          <Link href="/timeline" passHref>
+            <A>timeline</A>
+          </Link>
+          . Only 512 could deviate.
         </dd>
-        <dt>Where to start?</dt>
+        <dt>Chapter 2</dt>
+        <dd>[tbd but something with crafting 🔧🔥]</dd>
+        <dt>Can I play? Where to start?</dt>
         <dd>
           The best place to start is by checking out the "get-up-to-speed"
           channel in the{" "}
@@ -228,44 +223,16 @@ export default function About() {
               discord
             </A>
           </Link>
-          . Also, this video series:
-          <EmbeddedWrapper>
-            <blockquote
-              className="twitter-tweet"
-              data-dnt="true"
-              data-theme="dark"
-            >
-              <p lang="en" dir="ltr">
-                Working on a sort of &quot;documentary&quot; to help you get
-                into Corruption(s*),{" "}
-                <a href="https://twitter.com/dhof?ref_src=twsrc%5Etfw">@dhof</a>
-                &#39;s impossibly amazing on-chain pen &amp; paper art project.{" "}
-                <br />
-                <br />
-                Here&#39;s part 1 of... many to go. 😅
-                <br />
-                <br />
-                Watch this 🔊, find yours*, and join us here:
-                <a href="https://t.co/gDPzDnSQ7F">https://t.co/gDPzDnSQ7F</a>
-                <br />
-                <br />*
-                <a href="https://t.co/Bx831ZCZmw">
-                  https://t.co/Bx831ZCZmw
-                </a>{" "}
-                <a href="https://t.co/bJDYt7P3UQ">https://t.co/bJDYt7P3UQ</a>{" "}
-                <a href="https://t.co/VFPgFPe1xx">pic.twitter.com/VFPgFPe1xx</a>
-              </p>
-              &mdash; fabians.eth (😈, 😇) (@fabianstelzer){" "}
-              <a href="https://twitter.com/fabianstelzer/status/1470525607104684032?ref_src=twsrc%5Etfw">
-                December 13, 2021
-              </a>
-            </blockquote>{" "}
-            <script
-              async
-              src="https://platform.twitter.com/widgets.js"
-              charset="utf-8"
-            ></script>
-          </EmbeddedWrapper>
+          . Also, check out{" "}
+          <Link
+            href="https://twitter.com/fabianstelzer/status/1470525607104684032"
+            passHref
+          >
+            <A target="_blank" rel="noreferrer">
+              this video series
+            </A>
+          </Link>
+          .
         </dd>
       </Dl>
     </Section>
